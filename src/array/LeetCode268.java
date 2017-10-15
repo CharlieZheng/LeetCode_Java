dd0225149f0529bec159173449bef7f12f17fead
Zheng Hanrong <hanrongzheng@gmail.com>
2017-10-15 23:34:10 +0800

    Add LeetCode268.java and LeetCode268.class.

---
+++ src/array/LeetCode268.java
@@ -0,0 +1,19 @@
+package array;
+
+public class LeetCode268 {
+    public static void main(String[] args) {
+        LeetCode268 leetCode268 = new LeetCode268();
+       System.out.println( leetCode268.missingNumber(new int[]{0}));
+    }
+
+    public int missingNumber(int[] nums) {
+     Arrays.sort(nums);
+        int i =0;
+        for ( i = 0; i < nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+        return i;
+    }
+}
